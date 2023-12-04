6eac8c88a3efd1674b13fde7ade6e79d0c766c29
NullDivison <penumbra.scilicet@gmail.com>
2023-12-04 16:42:05 +0200

    day2-1: add calculations

--- day2/part1.hs
+++ day2/part1.hs
@@ -9,31 +9,43 @@ splitSets sep str =
 toInt :: String -> Integer
 toInt s = read s :: Integer
 
+getValue :: String -> String
+getValue s = head $ words s
+
 getLastWord :: String -> String
 getLastWord s = last $ words s
 
 toRgb :: (String, String, String) -> [String] -> (String, String, String)
 toRgb c [] = c
 toRgb (r, g, b) (s : sx)
-  | "red" == (getLastWord s) = toRgb (s, g, b) sx
-  | "green" == (getLastWord s) = toRgb (r, s, b) sx
-  | "blue" == (getLastWord s) = toRgb (r, g, s) sx
+  | "red" == (getLastWord s) = toRgb (getValue s, g, b) sx
+  | "green" == (getLastWord s) = toRgb (r, getValue s, b) sx
+  | "blue" == (getLastWord s) = toRgb (r, g, getValue s) sx
   | otherwise = toRgb (r, g, b) sx
 
 -- function gets called with a color set (eg. "1 red, 2 green, 3 blue")
-splitRgb :: String -> [(String, String, String)]
-splitRgb s = map(toRgb ("0", "0", "0") . words) . splitSets ',' $ s
+splitRgb :: String -> (String, String, String)
+splitRgb s = toRgb ("0", "0", "0") . splitSets ',' $ s
 
-splitParts :: [Char] -> (String, [[(String, String, String)]])
+toNumbers :: (String, String, String) -> (Integer, Integer, Integer)
+toNumbers (r, g, b) = (read r, read g, read b)
+
+isValidSet :: [(Integer, Integer, Integer)] -> Bool
+isValidSet [] = True
+isValidSet ((r, g, b) : sx)
+  | r > 12 || g > 13 || b > 14 = False
+  | otherwise = isValidSet sx
+
+splitParts :: [Char] -> (Integer, [(Integer, Integer, Integer)])
 splitParts s =
   let (game, cubes) = break (== ':') s
   -- we drop the ': ' part from cubes
   in (
-    game,
-    map(splitRgb) (splitSets ';' (drop 2 cubes))
+    toInt $ getLastWord game,
+    map(toNumbers . splitRgb) (splitSets ';' (drop 2 cubes))
   )
 
 main = do
   values <- readFile "./values.txt"
 
-  print . fmap(splitParts) . lines $ values
+  print . foldr (+) 0 . map(\(g, x) -> g) . filter (\(g, x) -> isValidSet x) . fmap(splitParts) . lines $ values
