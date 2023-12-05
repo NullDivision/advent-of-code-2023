91f9dea2c271d18478f1c41579b4b80c9d3d3f55
NullDivison <penumbra.scilicet@gmail.com>
2023-12-05 20:31:47 +0200

    day3-1: add boundaries

--- day3/day3.hs
+++ day3/day3.hs
@@ -1,6 +1,6 @@
 data Coordinate x y = Coordinate { x :: x, y :: y }
   deriving (Show)
-data TextBoundary a b = TextBoundary { start :: a, end :: b }
+data TextBoundary a b c = TextBoundary { start :: a, end :: b, text :: String }
   deriving (Show)
 
 createCoordinate :: x -> y -> Coordinate x y
@@ -22,13 +22,18 @@ reduceNumbers c ((y, v):sx) =
   else
     reduceNumbers c sx
 
+toBoundary :: Integer -> Integer -> String -> TextBoundary (Coordinate Integer Integer) (Coordinate Integer Integer) String
+toBoundary x y v = TextBoundary (Coordinate (x-1) (y-1)) (Coordinate (x+1) (y+(toInteger $ length v))) v
+
 main = do
   values <- readFile "./values.txt"
 
   let valueLines = lines values
   let matrixValues = zipWith (\x b -> (x, zipWith (\y v -> (Coordinate x y, v)) [0 :: Int ..] b)) [0 :: Int ..] $ valueLines
   let symbolCoordinates = (map (\(a, b) -> (a, filter (\(_, v) -> v `elem` "!@#$%^&*()_+-=") b)) $ matrixValues) >>= \(r, l) -> l
-  let rowsWithIndexes = zipWith (\x r -> (x, r)) [0 :: Int ..] valueLines
+  let rowsWithIndexes = zipWith (\x r -> (x, r)) [0 :: Integer ..] valueLines
+  let matrixText = map(\(x, r) -> (x, reduceNumbers [] $ zipWith (\y c -> (y, c)) [0 :: Integer ..] r)) $ rowsWithIndexes
+  let textBoundaries = (map (\(x, r) -> map (\(y, v) -> toBoundary x y v) r) $ matrixText) >>= \l -> l
 
-  print $ map(\(x, r) -> (x, reduceNumbers [] $ zipWith (\y c -> (y, c)) [0 :: Integer ..] r)) $ rowsWithIndexes 
 
+  print $ textBoundaries
