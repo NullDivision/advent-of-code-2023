6904e7fd35be6321f99414fb7722bc346921255b
NullDivison <penumbra.scilicet@gmail.com>
2023-12-06 14:49:03 +0200

    day3-1: finalize solution

--- day3/day3.hs
+++ day3/day3.hs
@@ -1,3 +1,6 @@
+import Data.List
+import Data.Maybe
+
 data Coordinate x y = Coordinate { x :: x, y :: y }
   deriving (Show)
 data TextBoundary a b c = TextBoundary { start :: a, end :: b, text :: String }
@@ -25,15 +28,20 @@ reduceNumbers c ((y, v):sx) =
 toBoundary :: Integer -> Integer -> String -> TextBoundary (Coordinate Integer Integer) (Coordinate Integer Integer) String
 toBoundary x y v = TextBoundary (Coordinate (x-1) (y-1)) (Coordinate (x+1) (y+(toInteger $ length v))) v
 
+findCoord :: [((Coordinate Integer Integer), Char)] -> (TextBoundary (Coordinate Integer Integer) (Coordinate Integer Integer) String) -> Bool
+findCoord c b = isJust $ find (\(co, _) -> ((x co) `elem` [(x $ start b)..(x $ end b)]) && ((y co) `elem` [(y $ start b)..(y $ end b)])) c
+
 main = do
   values <- readFile "./values.txt"
 
   let valueLines = lines values
-  let matrixValues = zipWith (\x b -> (x, zipWith (\y v -> (Coordinate x y, v)) [0 :: Int ..] b)) [0 :: Int ..] $ valueLines
-  let symbolCoordinates = (map (\(a, b) -> (a, filter (\(_, v) -> v `elem` "!@#$%^&*()_+-=") b)) $ matrixValues) >>= \(r, l) -> l
+  let matrixValues = zipWith (\x b -> (x, zipWith (\y v -> (Coordinate x y, v)) [0 :: Integer ..] b)) [0 :: Integer ..] $ valueLines
+  let symbolCoordinates = (map (\(a, b) -> (a, filter (\(_, v) -> v `elem` "!@#$%^&*()_+-=/") b)) $ matrixValues) >>= \(r, l) -> l
   let rowsWithIndexes = zipWith (\x r -> (x, r)) [0 :: Integer ..] valueLines
   let matrixText = map(\(x, r) -> (x, reduceNumbers [] $ zipWith (\y c -> (y, c)) [0 :: Integer ..] r)) $ rowsWithIndexes
   let textBoundaries = (map (\(x, r) -> map (\(y, v) -> toBoundary x y v) r) $ matrixText) >>= \l -> l
+  let findCoord' = findCoord $ symbolCoordinates
 
+  print $ foldr (+) 0 $ map (\b -> read (text b) :: Integer) $ filter (\b -> findCoord' b) textBoundaries
+  -- print $ map (\b -> read (text b) :: Integer) $ filter (\b -> findCoord' b) textBoundaries
 
-  print $ textBoundaries
